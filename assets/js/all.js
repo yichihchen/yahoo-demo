29f3066cc0f08ff51fc29737701e0f8bba0fa515
yichihchen <ingridiaim@gmail.com>
2023-03-11 15:42:39 +0800

    Update 2023-03-11T07:42:34.256Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,5 +1,24 @@
 "use strict";
 
-function openTabs(event, tabsType) {//讓所有tabs內容先消失
+function openTabs(event, tabsType) {
+  var i, panelsShifts, tabsLinks; //讓所有tabs內容先消失
+
+  panelsShifts = document.getElementsByClassName("panels-shifts");
+
+  for (i = 0; i < panelsShifts.length; i++) {
+    panelsShifts[i].style.display = "none"; //取得所有class="tabcontent" 先用js讓它隱藏
+  } //若html 按鈕active開起就讓它關閉
+
+
+  tabsLinks = document.getElementsByClassName("tabslinks");
+
+  for (i = 0; i < tabsLinks.length; i++) {
+    tabsLinks[i].className = tabsLinks[i].className.replace("active", "");
+  } //將active附加到其現有的className屬性值中
+  //id名稱要和function的tabsType能夠連結
+
+
+  document.getElementById(tabsType).style.display = "block";
+  event.currentTarget.className += "active";
 }
 //# sourceMappingURL=all.js.map
