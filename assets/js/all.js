866874340059890316399d6c100b9254cf7bfd72
yichihchen <ingridiaim@gmail.com>
2023-03-12 14:16:12 +0800

    Update 2023-03-12T06:16:06.912Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,8 +1,9 @@
 "use strict";
 
-function openTabs(event, tabsType) {
-  var i, panelsShifts, tabsLinks; //讓所有tabs內容先消失
+var i, panelsShifts, tabsLinks;
 
+function openTabs(event, tabsType) {
+  //讓所有tabs內容先消失
   panelsShifts = document.getElementsByClassName("panels-shifts");
 
   for (i = 0; i < panelsShifts.length; i++) {
